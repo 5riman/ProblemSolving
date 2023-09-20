8c09838d1e489f4e6c60014baec56655980b137b
5riman <cheetisriman@gmail.com>
2023-09-20 23:50:32 +0530

    N Even Numbers

---
+++ ProblemSolving/FirstNEven.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    public class FirstNEven
+    {
+        public static void Main(string[] args)
+        {
+            int N=0;
+            Console.WriteLine("Enter first N number");
+            N=Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<=N;i=i+2)
+            {
+               Console.Write(i);
+            }
+            
+        }
+    }
+}
