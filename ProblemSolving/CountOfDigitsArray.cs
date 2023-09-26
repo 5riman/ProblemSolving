c78e706ba6ac5eefbc40148de0e2dce903d9a89b
5riman <cheetisriman@gmail.com>
2023-09-26 15:50:44 +0530

    Todays Practice

---
+++ ProblemSolving/CountOfDigitsArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    internal class CountOfDigitsArray
+    {
+        static void Main(string[] carrot)
+        {
+            int count;
+            int[] A1 = new int[6] { 2, 54, 238, 2109876, 54, 2145 };
+            foreach (int X in A1)
+            {
+                count = 0;
+                int X1 = X;
+                while (X1 > 0)
+                {
+                    int R = X1 % 10;
+
+                    X1 = X1 / 10;
+                    count++;
+                }
+                Console.WriteLine("count of digits in an Array ELt :" + count);
+            }
+        }
+    }
+}
+    
---
+++ ProblemSolving/ReverseDigit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    internal class ReverseDigit
+    {
+        public static void Main(string[] args)
+        {
+            int N, Rnum = 0;
+            Console.WriteLine("Enter any Digit number");
+            N = Convert.ToInt32(Console.ReadLine());
+            while (N > 0)
+            {
+                int R = N % 10;
+                Rnum = Rnum * 10 + R;
+                N = N / 10;
+
+
+
+            }
+            Console.WriteLine("Reversed Number is " + Rnum);
+        }
+    }
+}
+    
+
