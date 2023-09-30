04ce004f30820efece8ca850c4c8a82866a4cb00
5riman <cheetisriman@gmail.com>
2023-09-30 07:56:43 +0530

    Daily practice

---
+++ ProblemSolving/Fibonoci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    internal class Fibonoci
+    {
+        public static void Main(string[] args)
+        {
+            int N, a = 0, b = 1, temp;
+            Console.WriteLine("Enter the range numbwer:");
+            N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                if (i <= 1)
+                {
+                    temp = i;
+                }
+                else
+                    temp = a + b;
+                a = b;
+                b = temp;
+                Console.WriteLine(temp + "");
+
+            }
+        }
+    }
+}
---
+++ ProblemSolving/PalindromeNested.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    internal class PalindromeNested
+    {
+        static void Main(string[] args)
+        {
+            int N, N1, Rnum, R;
+            for (N = 50; N <= 100; N++)
+            {
+                N1 = N;
+                Rnum = 0;
+                R = 0;
+
+                while (N1 > 0)
+                {
+
+                    R = N1 % 10;
+                    Rnum = Rnum * 10 + R;
+                    N1 = N1 / 10;
+
+
+                }
+                if (Rnum == N)
+                    Console.WriteLine(N + "");
+
+            }
+
+
+
+
+    
+}
+
+    }
+}
